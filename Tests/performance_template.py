9d492b643ed350ced3bc3f3b5c21aaac4f7e47d5
Dzhoharidze <Aleksey.Dzhoharidze@ontario.ca>
2023-08-17 15:39:24 -0400

    updated performance

---
+++ Tests/performance_template.py
@@ -0,0 +1,390 @@
+'''
+20 performance automation scenarios for a web app and APIs using Python and pytest. These scenarios cover various
+aspects of performance testing, such as load testing, stress testing, and response time testing.
+Remember to replace placeholders like YOUR_URL and YOUR_API_ENDPOINT with the appropriate URLs or
+endpoints for your application.
+
+'''
+
+import pytest
+import requests
+
+# Scenario 1: Load test the homepage of the web app
+@pytest.mark.performance
+def test_load_homepage():
+    response = requests.get("YOUR_URL")
+    assert response.status_code == 200
+
+# Scenario 2: Load test a specific page of the web app
+@pytest.mark.performance
+def test_load_specific_page():
+    response = requests.get("YOUR_URL/some_page")
+    assert response.status_code == 200
+
+# Scenario 3: Test API response time for a GET request
+@pytest.mark.performance
+def test_api_response_time():
+    response = requests.get("YOUR_API_ENDPOINT")
+    assert response.elapsed.total_seconds() < 1
+
+# Scenario 4: Test API response time for a POST request
+@pytest.mark.performance
+def test_api_post_response_time():
+    data = {"key": "value"}
+    response = requests.post("YOUR_API_ENDPOINT", json=data)
+    assert response.elapsed.total_seconds() < 2
+
+# Scenario 5: Stress test the web app homepage with multiple concurrent users
+@pytest.mark.performance
+def test_stress_homepage():
+    with pytest.threads(10):
+        response = requests.get("YOUR_URL")
+        assert response.status_code == 200
+
+# Scenario 6: Stress test a specific API endpoint with concurrent requests
+@pytest.mark.performance
+def test_stress_api_endpoint():
+    with pytest.threads(10):
+        response = requests.get("YOUR_API_ENDPOINT")
+        assert response.status_code == 200
+
+# Scenario 7: Test API throughput by sending a large number of requests
+@pytest.mark.performance
+def test_api_throughput():
+    for _ in range(100):
+        response = requests.get("YOUR_API_ENDPOINT")
+        assert response.status_code == 200
+
+# Scenario 8: Measure response time under high load for the homepage
+@pytest.mark.performance
+def test_high_load_homepage():
+    with pytest.threads(20):
+        response = requests.get("YOUR_URL")
+        assert response.elapsed.total_seconds() < 2
+
+# Scenario 9: Measure response time under high load for a specific API endpoint
+@pytest.mark.performance
+def test_high_load_api_endpoint():
+    with pytest.threads(20):
+        response = requests.get("YOUR_API_ENDPOINT")
+        assert response.elapsed.total_seconds() < 2
+
+# Scenario 10: Test API response time for multiple concurrent requests
+@pytest.mark.performance
+def test_concurrent_api_requests():
+    with pytest.threads(10):
+        response = requests.get("YOUR_API_ENDPOINT")
+        assert response.elapsed.total_seconds() < 1
+
+# Scenario 11: Test API response time for multiple concurrent POST requests
+@pytest.mark.performance
+def test_concurrent_api_posts():
+    data = {"key": "value"}
+    with pytest.threads(10):
+        response = requests.post("YOUR_API_ENDPOINT", json=data)
+        assert response.elapsed.total_seconds() < 2
+
+# Scenario 12: Test API response time distribution (percentiles)
+@pytest.mark.performance
+def test_api_response_percentiles():
+    response_times = []
+    for _ in range(50):
+        response = requests.get("YOUR_API_ENDPOINT")
+        response_times.append(response.elapsed.total_seconds())
+    assert max(response_times) < 2
+
+# Scenario 13: Test API response time under variable load
+@pytest.mark.performance
+def test_variable_load_api():
+    users = [5, 10, 15, 20]
+    for user_count in users:
+        with pytest.threads(user_count):
+            response = requests.get("YOUR_API_ENDPOINT")
+            assert response.status_code == 200
+
+# Scenario 14: Test API throughput under increasing load
+@pytest.mark.performance
+def test_increasing_load_api():
+    for user_count in range(1, 21):
+        with pytest.threads(user_count):
+            response = requests.get("YOUR_API_ENDPOINT")
+            assert response.status_code == 200
+
+# Scenario 15: Test API response time during a peak load
+@pytest.mark.performance
+def test_peak_load_api():
+    with pytest.threads(50):
+        response = requests.get("YOUR_API_ENDPOINT")
+        assert response.elapsed.total_seconds() < 3
+
+# Scenario 16: Test API response time during a prolonged load
+@pytest.mark.performance
+def test_prolonged_load_api():
+    with pytest.threads(10, duration=120):
+        response = requests.get("YOUR_API_ENDPOINT")
+        assert response.elapsed.total_seconds() < 2
+
+# Scenario 17: Test the maximum supported load for the web app
+@pytest.mark.performance
+def test_max_load_homepage():
+    with pytest.threads(100):
+        response = requests.get("YOUR_URL")
+        assert response.status_code == 200
+
+# Scenario 18: Test the maximum supported load for the API endpoint
+@pytest.mark.performance
+def test_max_load_api_endpoint():
+    with pytest.threads(100):
+        response = requests.get("YOUR_API_ENDPOINT")
+        assert response.status_code == 200
+
+# Scenario 19: Test API response time during gradual ramp-up
+@pytest.mark.performance
+def test_ramp_up_api():
+    for user_count in range(1, 31):
+        with pytest.threads(user_count):
+            response = requests.get("YOUR_API_ENDPOINT")
+            assert response.elapsed.total_seconds() < 2
+
+# Scenario 20: Test API response time during gradual ramp-down
+@pytest.mark.performance
+def test_ramp_down_api():
+    for user_count in range(30, 0, -1):
+        with pytest.threads(user_count):
+            response = requests.get("YOUR_API_ENDPOINT")
+            assert response.elapsed.total_seconds() < 2
+
+
+import pytest
+import requests
+
+# Scenario 21: Load test a specific page with dynamic content
+@pytest.mark.performance
+def test_load_dynamic_page():
+    response = requests.get("YOUR_URL/dynamic_page")
+    assert response.status_code == 200
+
+# Scenario 22: Stress test a login page with multiple concurrent users
+@pytest.mark.performance
+def test_stress_login_page():
+    with pytest.threads(10):
+        response = requests.get("YOUR_URL/login")
+        assert response.status_code == 200
+
+# Scenario 23: Test response time for loading a large image or file
+@pytest.mark.performance
+def test_large_file_load_time():
+    response = requests.get("YOUR_URL/large_file.png")
+    assert response.elapsed.total_seconds() < 3
+
+# Scenario 24: Measure response time under high load for a checkout process
+@pytest.mark.performance
+def test_high_load_checkout():
+    with pytest.threads(20):
+        response = requests.get("YOUR_URL/checkout")
+        assert response.elapsed.total_seconds() < 2
+
+# Scenario 25: Test page rendering time for a complex dashboard
+@pytest.mark.performance
+def test_dashboard_rendering():
+    response = requests.get("YOUR_URL/dashboard")
+    assert response.elapsed.total_seconds() < 5
+
+# Scenario 26: Test response time for submitting a form with various data
+@pytest.mark.performance
+def test_form_submission():
+    data = {"field1": "value1", "field2": "value2"}
+    response = requests.post("YOUR_URL/submit_form", data=data)
+    assert response.elapsed.total_seconds() < 2
+
+# Scenario 27: Test response time for search functionality
+@pytest.mark.performance
+def test_search_response_time():
+    response = requests.get("YOUR_URL/search?query=test")
+    assert response.elapsed.total_seconds() < 2
+
+# Scenario 28: Test response time during user registration process
+@pytest.mark.performance
+def test_user_registration():
+    data = {"username": "testuser", "email": "test@example.com"}
+    response = requests.post("YOUR_URL/register", data=data)
+    assert response.elapsed.total_seconds() < 3
+
+# Scenario 29: Measure response time for loading multiple pages in a sequence
+@pytest.mark.performance
+def test_page_sequence_response_time():
+    pages = ["page1", "page2", "page3"]
+    for page in pages:
+        response = requests.get(f"YOUR_URL/{page}")
+        assert response.elapsed.total_seconds() < 4
+
+# Scenario 30: Test response time for loading a cached page
+@pytest.mark.performance
+def test_cached_page_load_time():
+    response = requests.get("YOUR_URL/cached_page")
+    assert response.elapsed.total_seconds() < 1
+
+
+import pytest
+import requests
+
+# Scenario 31: Test response time for loading a product listing page
+@pytest.mark.performance
+def test_product_listing_response_time():
+    response = requests.get("YOUR_URL/products")
+    assert response.elapsed.total_seconds() < 3
+
+# Scenario 32: Test response time for loading a product details page
+@pytest.mark.performance
+def test_product_details_response_time():
+    response = requests.get("YOUR_URL/products/product_id")
+    assert response.elapsed.total_seconds() < 2
+
+# Scenario 33: Test response time for loading a blog post
+@pytest.mark.performance
+def test_blog_post_response_time():
+    response = requests.get("YOUR_URL/blog/post_id")
+    assert response.elapsed.total_seconds() < 2
+
+# Scenario 34: Test response time for loading a user profile
+@pytest.mark.performance
+def test_user_profile_response_time():
+    response = requests.get("YOUR_URL/users/user_id")
+    assert response.elapsed.total_seconds() < 2
+
+# Scenario 35: Test response time for loading a category page
+@pytest.mark.performance
+def test_category_page_response_time():
+    response = requests.get("YOUR_URL/categories/category_id")
+    assert response.elapsed.total_seconds() < 3
+
+# Scenario 36: Test response time for loading the contact page
+@pytest.mark.performance
+def test_contact_page_response_time():
+    response = requests.get("YOUR_URL/contact")
+    assert response.elapsed.total_seconds() < 2
+
+# Scenario 37: Test response time for loading the about page
+@pytest.mark.performance
+def test_about_page_response_time():
+    response = requests.get("YOUR_URL/about")
+    assert response.elapsed.total_seconds() < 2
+
+# Scenario 38: Test response time for loading the FAQ page
+@pytest.mark.performance
+def test_faq_page_response_time():
+    response = requests.get("YOUR_URL/faq")
+    assert response.elapsed.total_seconds() < 2
+
+# Scenario 39: Test response time for loading the pricing page
+@pytest.mark.performance
+def test_pricing_page_response_time():
+    response = requests.get("YOUR_URL/pricing")
+    assert response.elapsed.total_seconds() < 2
+
+# Scenario 40: Test response time for loading the login page
+@pytest.mark.performance
+def test_login_page_response_time():
+    response = requests.get("YOUR_URL/login")
+    assert response.elapsed.total_seconds() < 2
+
+# Scenario 41: Test response time for loading the registration page
+@pytest.mark.performance
+def test_registration_page_response_time():
+    response = requests.get("YOUR_URL/register")
+    assert response.elapsed.total_seconds() < 2
+
+# Scenario 42: Test response time for loading a featured content section
+@pytest.mark.performance
+def test_featured_section_response_time():
+    response = requests.get("YOUR_URL/featured")
+    assert response.elapsed.total_seconds() < 3
+
+# Scenario 43: Test response time for loading a trending content section
+@pytest.mark.performance
+def test_trending_section_response_time():
+    response = requests.get("YOUR_URL/trending")
+    assert response.elapsed.total_seconds() < 3
+
+# Scenario 44: Test response time for loading a recommended products section
+@pytest.mark.performance
+def test_recommendations_section_response_time():
+    response = requests.get("YOUR_URL/recommended")
+    assert response.elapsed.total_seconds() < 3
+
+# Scenario 45: Test response time for loading the search results page
+@pytest.mark.performance
+def test_search_results_response_time():
+    response = requests.get("YOUR_URL/search?query=test")
+    assert response.elapsed.total_seconds() < 3
+
+# Scenario 46: Test response time for loading the cart page
+@pytest.mark.performance
+def test_cart_page_response_time():
+    response = requests.get("YOUR_URL/cart")
+    assert response.elapsed.total_seconds() < 2
+
+# Scenario 47: Test response time for adding a product to the cart
+@pytest.mark.performance
+def test_add_to_cart_response_time():
+    response = requests.post("YOUR_URL/cart/add", data={"product_id": "123"})
+    assert response.elapsed.total_seconds() < 2
+
+# Scenario 48: Test response time for updating cart quantity
+@pytest.mark.performance
+def test_update_cart_quantity_response_time():
+    response = requests.post("YOUR_URL/cart/update", data={"product_id": "123", "quantity": 2})
+    assert response.elapsed.total_seconds() < 2
+
+# Scenario 49: Test response time for removing a product from the cart
+@pytest.mark.performance
+def test_remove_from_cart_response_time():
+    response = requests.post("YOUR_URL/cart/remove", data={"product_id": "123"})
+    assert response.elapsed.total_seconds() < 2
+
+# Scenario 50: Test response time for completing a purchase
+@pytest.mark.performance
+def test_purchase_response_time():
+    response = requests.post("YOUR_URL/checkout", data={"total_amount": "100"})
+    assert response.elapsed.total_seconds() < 3
+
+
+
+'''
+Performance testing for web applications involves evaluating the responsiveness, speed, stability, scalability,
+ and overall reliability of the application under various conditions. It aims to ensure that the web app performs 
+ optimally, even under high user loads or stressful situations, and meets user expectations for speed and responsiveness. 
+ Performance testing helps identify bottlenecks, potential issues, and areas for optimization, ultimately contributing to
+  an enhanced user experience.
+
+Key Performance Testing Criteria:
+Response Time: Measures the time taken by the application to respond to user actions. It's crucial to ensure that the application provides fast and responsive interactions.
+Throughput: Evaluates the rate at which the application can handle and process requests or transactions. Higher throughput indicates better processing capacity.
+Load Testing: Simulates real-world user load to assess how well the application performs under expected and peak user traffic. Load testing helps determine if the app can handle the expected user load without performance degradation.
+Stress Testing: Pushes the application beyond its normal operating conditions to identify its breaking point and determine the maximum user load it can handle before failing. This helps in understanding system limitations.
+Scalability Testing: Tests the application's ability to handle increased load by adding resources, such as servers, and assessing how well it scales horizontally or vertically.
+Capacity Testing: Determines the maximum capacity of the application in terms of concurrent users or transactions it can handle before performance degrades significantly.
+Endurance or Soak Testing: Involves subjecting the application to a sustained load over an extended period to identify memory leaks, resource exhaustion, and other issues that might occur under prolonged usage.
+Concurrency Testing: Assesses how well the application handles multiple users accessing the same or different parts of the application simultaneously.
+Baseline Testing: Establishes a performance baseline by conducting tests in a controlled environment to measure the application's normal performance metrics.
+Peak Load Testing: Tests the application's performance under anticipated peak load conditions to ensure it can handle spikes in user activity without major performance issues.
+Failover and Recovery Testing: Assesses the application's ability to recover gracefully from failures, crashes, or other adverse events without compromising user experience.
+Network Latency Testing: Evaluates the impact of network delays on the application's performance, especially for users accessing the app from different geographic locations.
+Resource Utilization Testing: Monitors and analyzes the consumption of system resources (CPU, memory, disk space, etc.) during different types of testing scenarios.
+Third-party Integration Testing: Assesses how well the application interacts with external services, APIs, databases, or third-party components, ensuring they don't negatively impact performance.
+User Experience Testing: Focuses on the user's perspective by measuring user satisfaction, user interface responsiveness, and overall usability during performance tests.
+Browser Compatibility Testing: Evaluates how well the application performs across different web browsers, ensuring a consistent experience for users.
+Caching and Content Delivery Testing: Tests the efficiency of caching mechanisms and content delivery networks (CDNs) in reducing load times and improving overall performance.
+Security Performance Testing: Assesses the performance impact of security mechanisms, such as firewalls, encryption, and authentication processes.
+Database Performance Testing: Evaluates the responsiveness and efficiency of database queries, transactions, and data retrieval operations.
+Real-world Scenario Testing: Simulates real-world usage scenarios, including user actions, transactions, and workflows, to assess overall system behavior.
+Effective performance testing requires a combination of tools, methodologies, and skilled professionals to design, execute, and analyze the tests. The ultimate goal is to ensure that the web application can handle different types of loads, deliver a satisfactory user experience, and meet performance expectations in production environments.
+'''
+
+
+
+
+
+
+
+
