fc55b2889bfc6c57cf233a2cb22f089617543d82
Dzhoharidze <Aleksey.Dzhoharidze@ontario.ca>
2023-08-18 15:41:18 -0400

    pipeline updated

--- Tests/test_suit.py
+++ Tests/test_suit.py
@@ -6,12 +6,12 @@ def test_full_training_program_creation_JHSC():
     openweb(portal_link)
     print('link connected')
     logging(user, password) is True, 'Failed to log in'
-    assert subbmittionoftrainingprogram(JHSCPartOne) is True, 'Failed to submit information'
-    assert checkboxes(JHSCPartOne) is True, 'Failed to fill up checkboxes'
-    assert trainingprogramsmaterials() is True, 'Failed to fill up materials'
-    assert uploadfiles() is True, 'Failed to upload files'
-    assert applicationreview() is True, 'Failed to review app'
-    assert submitapp() is True, 'Failed to submit final app'
+    subbmittionoftrainingprogram(JHSCPartOne) is True, 'Failed to submit information'
+    checkboxes(JHSCPartOne) is True, 'Failed to fill up checkboxes'
+    trainingprogramsmaterials() is True, 'Failed to fill up materials'
+    uploadfiles() is True, 'Failed to upload files'
+    applicationreview() is True, 'Failed to review app'
+    submitapp() is True, 'Failed to submit final app'
     assert successcheck() is True, 'Failed to check success'
     # newprogramid = programid()
     runtime()
