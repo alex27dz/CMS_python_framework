11f51b732e318417945cb4b3c1691e06ee0cb64c
Dzhoharidze <Aleksey.Dzhoharidze@ontario.ca>
2023-09-04 22:40:13 -0400

    updated scenarios

--- Tests/new_sql_connector_microsoft.py
+++ Tests/new_sql_connector_microsoft.py
@@ -1,32 +1,25 @@
-import pyodbc
+import pyodbc  # Open Database Connectivity
 
 # Define the connection string parameters
-server_name = "your_server_name"  # Replace with your SQL Server's hostname
-database_name = "your_database_name"  # Replace with your database name
-username = "your_username"  # Replace with your SQL Server username
-password = "your_password"  # Replace with your SQL Server password
+server_name = "ltc-ist-08-cc-0019-sat-sql-ewc3a.database.windows.net,1433"  # Replace with your SQL Server's hostname
+database_name = "CMS_QA"  # Replace with your database name
+username = "CMSIstUser"  # Replace with your SQL Server username
+password = "j22WaVeDPUM4S3K9"  # Replace with your SQL Server password
 
 # Create a connection string
 connection_string = f"Driver={{ODBC Driver 17 for SQL Server}};Server={server_name};Database={database_name};Uid={username};Pwd={password}"
 
-try:
-    # Establish a connection to the SQL Server database
-    conn = pyodbc.connect(connection_string)
+# Establish a connection to the database
+connection = pyodbc.connect(connection_string)
+cursor = connection.cursor()  # Create a cursor to interact with the database
+cursor.execute("SELECT * FROM TrainingProviderApplicantBusinesses")  # Execute SQL queries
+rows = cursor.fetchall()
+for row in rows:  # Iterate through the results
+    print(row)
+cursor.close()  # Close the cursor and connection when done
+connection.close()
 
-    # Create a cursor object for executing SQL queries
-    cursor = conn.cursor()
 
-    # Example: Execute a simple query
-    cursor.execute("SELECT * FROM YourTable")
 
-    # Fetch and print the result
-    rows = cursor.fetchall()
-    for row in rows:
-        print(row)
 
-    # Close the cursor and connection when done
-    cursor.close()
-    conn.close()
 
-except pyodbc.Error as e:
-    print("Error connecting to the database:", e)
