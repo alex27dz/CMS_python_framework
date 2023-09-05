6c041683f5f08461ee718ad0cbaed8a6e79e8856
Dzhoharidze <Aleksey.Dzhoharidze@ontario.ca>
2023-09-05 17:33:29 -0400

    updated APIs

--- Tests/new_api.py
+++ Tests/new_api.py
@@ -1,4 +1,3 @@
-
 '''
 API requests are the mechanisms by which one software application sends a request to another application's API to perform certain actions or retrieve specific information
 
@@ -38,10 +37,10 @@ https://netsdc.visualstudio.com/SDC/_workitems/edit/98372	5	CMS API: Class Offer
 https://netsdc.visualstudio.com/SDC/_workitems/edit/98194	6	CMS API: Class Offerings - Add
 '''
 
-
 import requests
 import api
 import pytest
+
 
 def postman_API():
     print('Postman API')
@@ -67,6 +66,47 @@ def postman_API():
     print(response.status_code)  # 200 success
     # print(response.json())
     print(response.text)
+
+def add_class_offerings_98194():
+    print('api_class_offerings_add')
+    url = "https://intra.stage.apps.labour.gov.on.ca/api-facade-qa/ClassOffering"
+    headers = {
+        "Authorization": "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwia2V5IjoiRTkzM0QxQjMtMzQwNC00RUI1LUE3MEYtQjIxMjhCM0EyQzZBIn0.f3daVM-MZy8idhskfmafSdcZw6mwIiEzBze7UCZ2V6A",
+        "Content-Type": "application/json"
+    }
+
+    body = {
+      "offeringId": "wah-999",
+      "trainingStandardKey": "WAH-10083",
+      "deliveryMethod": "in-person",
+      "courseName": "Working At Heights",
+      "seatsRemaining": 5,
+      "contactForPricing": True,
+      "price": 100,
+      "address": {
+        "street-address": "1230 Main Street",
+        "extended-address": "PO Box 1234",
+        "locality": "Toronto",
+        "region": "ON",
+        "postal-code": "M7A 1T7",
+        "country-name": "Canada"
+      },
+      "events": [
+        {
+          "start": "2023-09-05T10:30:00.000Z",
+          "end": "2023-09-05T12:00:00.000Z"
+        }
+      ],
+      "externalRegistrationUrl": "https://training-provider.com/reserve-seats/10012",
+      "virtualClassUrl": "https://virtual-class-example.com/10001"
+    }
+
+    response = requests.post(url, json=body, headers=headers)
+    print(response.status_code)  # 204
+    # print(response.json())
+    print(response.text)
+# add_class_offerings_98194()
+
 def add_learning_record_api_98393():
     print('Add Learning Record API')
     url = "https://intra.stage.apps.labour.gov.on.ca/api-facade-qa/LearningRecord"
@@ -76,14 +116,14 @@ def add_learning_record_api_98393():
     }
     body = {
       "learningRecordId": "4108123e-7f35-4097-928a-5bccd5fe4111",
-      "trainingStandardKey": "WAH-E-B",
-      "completionDate": "2018-11-21T22:38:15.000Z",
+      "trainingStandardKey": "WAH-10083",
+      "completionDate": "2023-09-05T22:38:15.000Z",
       "networkKey": "pro-34616",
-      "externalClassId": "WAH-10083",  # Need to match the ID of the class generated using the API's ClassOffering endpoint
+      "externalClassId": "wah-999",  # Need to match the ID of the class generated using the API's ClassOffering endpoint
       "instructorNames": "Jane Doe; Dave Weir",
       "evaluatorNames": "John Smith",
       "learner": {
-        "uniqueId": "user-111",
+        "uniqueId": "user-999",
         "firstname": "Peter",
         "lastname": "Johnson",
         "personalEmail": "perter.johnson@example.com",
@@ -105,46 +145,11 @@ def add_learning_record_api_98393():
     print(response.status_code)  # 204
     # print(response.json())
     print(response.text)
-add_learning_record_api_98393()
-def api_class_offerings_add_98194():
-    print('api_class_offerings_add')
-    url = "https://intra.stage.apps.labour.gov.on.ca/api-facade-qa/ClassOffering"
-    headers = {
-        "Authorization": "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwia2V5IjoiRTkzM0QxQjMtMzQwNC00RUI1LUE3MEYtQjIxMjhCM0EyQzZBIn0.f3daVM-MZy8idhskfmafSdcZw6mwIiEzBze7UCZ2V6A",
-        "Content-Type": "application/json"
-    }
+# add_learning_record_api_98393()
 
-    body = {
-      "offeringId": "wah-2727",
-      "trainingStandardKey": "WAH-10083",
-      "deliveryMethod": "in-person",
-      "courseName": "Working At Heights",
-      "seatsRemaining": 5,
-      "contactForPricing": True,
-      "price": 100,
-      "address": {
-        "street-address": "1230 Main Street",
-        "extended-address": "PO Box 1234",
-        "locality": "Toronto",
-        "region": "ON",
-        "postal-code": "M7A 1T7",
-        "country-name": "Canada"
-      },
-      "events": [
-        {
-          "start": "2018-11-21T10:30:00.000Z",
-          "end": "2018-11-21T12:00:00.000Z"
-        }
-      ],
-      "externalRegistrationUrl": "https://training-provider.com/reserve-seats/10012",
-      "virtualClassUrl": "https://virtual-class-example.com/10001"
-    }
 
-    response = requests.post(url, json=body, headers=headers)
-    print(response.status_code)  # 204
-    # print(response.json())
-    print(response.text)
-# api_class_offerings_add_98194()
+
+
 
 
 
